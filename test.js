7a95a7240d2fbfa673e680663c4065c504a6a285
Istrimin <devenal@email.com>
2024-08-30 07:32:49 +0800

    fix

--- test.js
+++ test.js
@@ -1,38 +1,25 @@
-// let lastDrawTime = 0;
-// let lastX, lastY;
-let lastX=0;
-let lastY=0;
 
+let lastX = 0;
+let lastY = 0;
 
-// function startDrawing(e) {
-//     e.preventDefault();
-//     isDrawing = true;
-//     points = [];
-//     points.push({
-//         x: e.offsetX,
-//         y: e.offsetY,
-//         pressure: e.pressure || 1
-//     });
-//     saveState();
-//     e.target.setPointerCapture(e.pointerId);
-// }
 canvas.addEventListener('pointerdown', startDrawing, { passive: false });
 canvas.addEventListener('pointermove', draw);
 canvas.addEventListener('pointerup', stopDrawing);
 canvas.addEventListener('pointerout', stopDrawing);
 canvas.addEventListener('pointercancel', stopDrawing);
 
-// Для поддержки сенсорных устройств
 canvas.addEventListener('touchstart', (e) => {
     e.preventDefault();
     const touch = e.touches[0];
     startDrawing({offsetX: touch.clientX - canvas.offsetLeft, offsetY: touch.clientY - canvas.offsetTop});
-});
+}, { passive: false });
+
 canvas.addEventListener('touchmove', (e) => {
     e.preventDefault();
     const touch = e.touches[0];
     draw({offsetX: touch.clientX - canvas.offsetLeft, offsetY: touch.clientY - canvas.offsetTop});
-});
+}, { passive: false });
+
 canvas.addEventListener('touchend', stopDrawing);
 
 
@@ -40,6 +27,7 @@ function startDrawing(e) {
     isDrawing = true;
     [lastX, lastY] = [e.offsetX, e.offsetY];
 }
+
 function draw(e) {
     if (!isDrawing) return;
     e.preventDefault();
@@ -77,44 +65,5 @@ function draw(e) {
 
 function stopDrawing() {
     isDrawing = false;
+    console.log("Stop Drawing");
 }
-// canvas.addEventListener('mousedown', startDrawing);
-// canvas.addEventListener('mousemove', draw);
-// canvas.addEventListener('mouseup', stopDrawing);
-// canvas.addEventListener('mouseout', stopDrawing);
-
-
-
-
-// function draw(e) {
-//     if (!isDrawing) return;
-//     e.preventDefault();
-//     const x = e.offsetX;
-//     const y = e.offsetY;
-//     const pressure = e.pressure || 1;
-//     ctx.lineWidth = brushSizeInput.value * pressure;
-//     ctx.lineCap = 'round';
-//     ctx.lineJoin = 'round';
-//     ctx.strokeStyle = isEraser ? backgroundPicker.value : colorPicker.value;
-//     ctx.globalAlpha = opacityInput.value / 100;
-//     ctx.beginPath();
-//     ctx.moveTo(lastX, lastY);
-//     ctx.lineTo(x, y);
-//     ctx.stroke();
-//     if (symmetry) {
-//         const centerX = canvas.width / 2;
-//         ctx.save();
-//         ctx.scale(-1, 1);
-//         ctx.translate(-canvas.width, 0);
-//         ctx.beginPath();
-//         ctx.moveTo(2 * centerX - lastX, lastY);
-//         ctx.lineTo(2 * centerX - x, y);
-//         ctx.stroke();
-//         ctx.restore();
-//     }
-//     lastX = x;
-//     lastY = y;
-// }
-// function stopDrawing() {
-//     isDrawing = false;
-// }
